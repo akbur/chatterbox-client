efceb115358f717075aeddefe0fff8ba2031f344
Kris Burke <kris.burke611@gmail.com>
2016-01-26 00:45:48 -0500

    Added basic layout with method names based on spec.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,36 @@
-// YOUR CODE HERE:
+var app = {
+  server: 'https://api.parse.com/1/classes/chatterbox',
+  username: 'anonymous',
+};
 
+app.init = function() {
+
+};
+ 
+app.send = function() {
+
+};
+
+app.fetch = function() {
+
+};
+
+app.clearMessages = function() {
+
+};
+
+app.addMessage = function() {
+
+};
+
+app.addRoom = function() {
+
+};
+
+app.addFriend = function() {
+
+};
+
+app.handleSubmit = function() {
+
+};
