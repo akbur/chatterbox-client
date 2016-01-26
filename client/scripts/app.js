6eedc59da475e82266e6d60d44e8356138c5d2ad
Kris Burke <kris.burke611@gmail.com>
2016-01-26 00:49:44 -0500

    Added a send method.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,7 +7,19 @@ app.init = function() {
 
 };
  
-app.send = function() {
+app.send = function(message) {
+  $.ajax({
+    url: app.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function(data) {
+      console.log('chatterbox: Message sent. Data: ', data);
+    }, 
+    error: function(data) {
+      console.error('chatterbox: Failed to send message. Error: ', data);
+    },
+  });
 
 };
 
