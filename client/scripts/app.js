7d642f59a9e2c40eea2f02ef482924f565f1671c
Kris Burke <kris.burke611@gmail.com>
2016-01-26 14:52:54 -0500

    Added handleSubmit method along with a couple of helper functions for sending messages.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,7 +2,7 @@ var app = {
   server: 'https://api.parse.com/1/classes/chatterbox',
   username: 'anonymous',
   rooms: {},
-  friends: [],
+  friends: {},
 };
 
 app.init = function() {
@@ -31,6 +31,7 @@ app.fetch = function() {
     url: app.server, 
     type: 'GET',
     contentType: 'application/json',
+    data: {order: '-createdAt'},
     success: function(data) {
       app.processRoomData(data);
       app.processMessageData(data);
@@ -59,41 +60,60 @@ app.processMessageData = function(data) {
     _.each(data.results, function(message) {
       var text = message.text;
       var username = message.username;
+      var fromFriend = app.messageFromFriend(message);
       if (text && username) {
-        app.addMessage(username, text);
+        app.addMessage(username, text, fromFriend);
       }
     });  
   }
+}
+
+app.addFriendClickHandler = function() {
+  $('.username').on('click', app.addFriend);
+}
+
+app.addFriend = function() {
+  name = $(this).text();
+  app.friends[name] = true;
+};
+
+app.messageFromFriend = function(message) {
+  return (message.username in app.friends);
 }
 
 app.clearMessages = function() {
   $('#chats').empty();
 };
 
-app.addMessage = function(username, text) {
+app.addMessage = function(username, text, fromFriend) {
   var message = '<div class="chat"><span class="username">';
   message += username + '</span><br>' + text + '</div>';
   $('#chats').append(message);
 };
-
-
-app.addFriendClickHandler = function() {
-  $('.username').on('click', app.addFriend);
-}
-
 
 app.addRoom = function(roomname) {
   var option = '<option value="' + roomname + '">' + roomname + '</option>';
   $('#roomSelect').append(option);
 };
 
+app.createMessage = function() {
+  var message = {};
+  message.text = $('#message').val();
+  message.username = app.username;
+  //TODO: get selected room
+  message.room = 'lobby';
+  return message;
+}
 
-app.addFriend = function() {
-  app.friends.push($(this));
-};
+app.clearMessage = function() {
+  $('#message').val('');
+}
 
-app.handleSubmit = function() {
-
+app.handleSubmit = function(e) {
+  e && e.preventDefault();
+  var message = app.createMessage();
+  app.send(message);
+  app.clearMessage();
 };
 
 app.init();
